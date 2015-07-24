88c19037e0488c9c94cf618e38efe4ec9b9d854f
Sam Stiles <samuel.dev.stiles@gmail.com>
2015-07-23 21:51:54 -0300

    Likely committing broken code but idgaf cuz im a boss :goat:

--- routes/climb.js
+++ routes/climb.js
@@ -1,77 +1,90 @@
+'use strict';
+
 var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
 
+function findAllClimbs () {
+  return models.Climb.findAll({
+    include: [{
+      model: models.Photo,
+      as: 'Photos'
+    }, {
+      model: models.Sublocation
+    }]
+  });
+}
+
+function createClimb (data) {
+  return models.Climb.create(req.body).then(function(climb) {
+    return findClimbById(climb.dataValues.id);
+  }, errorHandler;
+}
+
+function findClimbById (id) {
+  return models.Climb.findById(id, {
+    include: [{
+      model: models.Photo,
+      as: 'Photos'
+    }, {
+      model: models.Sublocation
+    }]
+  });
+}
+
+function updateClimbById (id, data) {
+  return models.Climb.update(data, {
+    where: { id: id }
+  }).then(function() {
+    return findClimbById(id);
+  }, errorHandler);
+}
+
+function deleteClimbById (id) {
+  return models.Climb.destroy({
+    where: {
+      id: id
+    }
+  });
+}
+
+function errorHandler (error) {
+  // Parse the error object and send a 400 if we know the error has something
+  // to do with the data the user submitted, otherwise send 500 because the
+  // error is probably ours :)
+  return error.name === 'SequelizeValidationError' ? res.send(400, error) : res.send(500, error);
+}
+
+function successHandler (resource) {
+  // Send back the resource that was updated/created/retrieved etc etc
+  return res.send(200, resource);
+}
+
+/* Base level (id-less) routes */
 router.route('/')
+
   .get(function(req, res) {
-    models.Climb.findAll({
-    include: [ { model: models.Photo, as: 'Photos' }, { model: models.Sublocation } ]
-  }).then(function(climbs) {
-      res.send(climbs);
-    }, function(err) {
-      console.log(err);
-      res.sendStatus(500);
-    });
+    findAllClimbs.then(successHandler, errorHandler);
   })
 
   .post(function(req, res) {
-    models.Climb.create(req.body).then(function(climb) {
-      models.Climb.findById(climb.dataValues.id, {
-        include: [ { model: models.Photo, as: 'Photos' }, { model: models.Sublocation } ]
-      }).then(function(climb) {
-        res.send(climb);
-      }, function(err) {
-        console.log(err);
-        res.sendStatus(500);
-      })
-    }, function(err) {
-      console.log(err);
-      res.send(400, err);
-    });
+    createClimb(req.body).then(successHandler, errorHandler);
   });
 
 
-router.route('/:climb_id')
+/* Routes Where ID is specified*/
+router.route('/:climbId')
 
   .get(function(req, res) {
-    models.Climb.findById(req.params.climb_id, {
-      include: [ { model: models.Photo, as: 'Photos' }, { model: models.Sublocation } ]
-    }).then(function(climb) {
-      res.send(climb);
-    }, function(err) {
-      console.log(err);
-      res.sendStatus(500);
-    });
+    findClimbById(req.params.climbId).then(successHandler, errorHandler);
   })
 
   .put(function(req, res) {
-    models.Climb.update(req.body, {
-      where: { id: req.params.climb_id }
-    }).then(function() {
-      models.Climb.findById(req.params.climb_id, {
-        include: [ { model: models.Photo, as: 'Photos' }, { model: models.Sublocation } ]
-      }).then(function(climb) {
-        res.send(climb);
-      }, function(err) {
-        console.log(err);
-        res.sendStatus(500);
-      })
-    }, function(err) {
-      console.log(err);
-      res.sendStatus(500);
-    });
+    updateClimbById(req.body, req.params.climbId).then(successHandler, errorHandler);
   })
 
   .delete(function(req, res) {
-    models.Climb.destroy(
-      {where: { id: req.params.climb_id }
-    }).then(function() {
-      res.sendStatus(200);
-    }, function(err) {
-      console.log(err);
-      res.sendStatus(500);
-    });
+    deleteClimbById(req.params.climbId).then(successHandler, errorHandler);
   });
-
 
 module.exports = router;
