c2159e6d2bcfed37801ff111f73470dc14513b8f
n9i5a <leahbelyea@gmail.com>
2015-07-26 22:47:14 -0300

    add more db data, allow cors

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ var country = require('./routes/country');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', routes);
 app.use('/climb', climb);
 app.use('/sublocation', sublocation);
